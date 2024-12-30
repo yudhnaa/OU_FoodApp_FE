025529cec82f495653bed39d4961561b6df1acd2
yudhna <hoanganhduy75@gmail.com>
2024-12-30 22:49:54 +0700

    ptofile page

--- FoodApp/app/_layout.tsx
+++ FoodApp/app/_layout.tsx
@@ -5,6 +5,7 @@ export default function RootLayout() {
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="(welcome)" options={{ headerShown: false }} />
+      <Stack.Screen name="profile_menu" options={{ headerShown: false }} />
     </Stack>
   );
 }
--- FoodApp/app/index.tsx
+++ FoodApp/app/index.tsx
@@ -31,7 +31,7 @@ export default function Home() {
 
   useEffect(() => {
     if (fontsLoaded) {
-      router.replace("/loading");
+      router.replace("/profile_menu");
     }
   }, [fontsLoaded]);
 
