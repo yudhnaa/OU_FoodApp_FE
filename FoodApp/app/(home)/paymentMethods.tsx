9658ac762fb171c651322766b950ede7cd989100
yudhna <you@example.com>
2025-01-29 19:22:04 +0700

    done get, del payment methods

--- FoodApp/app/(home)/paymentMethods.tsx
+++ FoodApp/app/(home)/paymentMethods.tsx
@@ -7,70 +7,101 @@ import Colors from "@/styles/colors";
 import fontStyles from "@/styles/fontStyles";
 import {router} from "expo-router";
 import Button from "@/components/home/button";
+import {authApi, endpoints} from "@/configs/APIs";
+import {useAuth} from "@/components/AuthContext";
+import {LoadingOverlay} from "@/components/home/LoadingComponents";
 
-type PaymentMethod = {
-    id: number;
-    methodName: string;
-    icon: any;
-    isDefault: boolean;
-};
-
-const paymentMethods = [
-    {
-        id: 1,
-        methodName: "Credit card: *** *** *** 43",
-        icon: require("@/assets/images/icons/ico_card.svg"),
-        isDefault: true
-    },
-    {
-        id: 2,
-        methodName: "Apple Pay: *** *** *** 43",
-        icon: require("@/assets/images/icons/ico_apple.svg"),
-        isDefault: false
-    },
-    {
-        id: 3,
-        methodName: "Paypal: *** *** *** 43",
-        icon: require("@/assets/images/icons/ico_paypal.svg"),
-        isDefault: false
-    },
-    {
-        id: 4,
-        methodName: "Google Pay: *** *** *** 43",
-        icon: require("@/assets/images/icons/ico_ggpay.svg"),
-        isDefault: false
-    }]
 
 const PaymentMethods = () => {
-
-    const [defaultMethod, setDefaultMethod] = useState<PaymentMethod>();
-
-    const setDefault = (method: PaymentMethod) => {
-        if (method) {
-            if (defaultMethod) {
-                defaultMethod.isDefault = false;
-            }
-            method.isDefault = true;
-            setDefaultMethod(method);
-        }
+    const iconMap: { [key: string]: any } = {
+        momo: require("@/assets/images/icons/ico_momo.svg"),
+        zalopay: require("@/assets/images/icons/ico_zalopay.svg"),
+        paypal: require("@/assets/images/icons/ico_paypal.svg"),
+        stripe: require("@/assets/images/icons/ico_stripe.svg"),
+        cash: require("@/assets/images/icons/ico_cash.svg"),
     };
+
+    type PaymentMethods = {
+        "id": number,
+        "user": number,
+        "payment_type": string,
+        "created_at": string,
+        "isActive": boolean,
+        "isDefault": boolean,
+        "icon": string
+    };
+
+    const [paymentMethods, setPaymentMethods] = useState<PaymentMethods[]>([]);
+    const [defaultMethod, setDefaultMethod] = useState<PaymentMethods>();
+    const {access_token} = useAuth();
+    const [loading, setLoading] = useState(false);
+
+    const setDefault = (method: PaymentMethods) => {
+        setLoading(true);
+        authApi(access_token).post(endpoints.set_default_payment_method, {
+            "user_payment_id": method.id
+        }).then((res) => {
+            if (method.payment_type === res.data.payment_type) {
+                if (defaultMethod) {
+                    defaultMethod.isDefault = false;
+                }
+                method.isDefault = true;
+                setDefaultMethod(method);
+            }
+
+        }).catch(ex => {
+            alert(ex.response.data?.error_description || "Loading failed\nStatus code" + ex.status)
+        }).finally(() => {
+            setLoading(false);
+        })
+    };
+
+    useEffect(() => {
+        const getPaymentMethods = async () => {
+            setLoading(true);
+            authApi(access_token).get(endpoints.payment_methods).then((res) => {
+                const methods = res.data.payment_methods.map((method: PaymentMethods) => {
+                    const iconKey = method.payment_type.toLowerCase().replace(" ", "_");
+                    method.icon = iconMap[iconKey] || null;
+                    return method;
+                });
+                setPaymentMethods(methods);
+            }).catch(ex => {
+                alert(ex.response.data?.error_description || "Get payment methods failed\nStatus code" + ex.status)
+            }).finally(() => {
+                setLoading(false);
+            })
+        }
+        getPaymentMethods()
+    }, [])
 
     useEffect(() => {
         paymentMethods.find((method) => {
             if (method.isDefault) {
                 setDefaultMethod(method);
             }
-        })
+        });
+    }, [paymentMethods]);
 
-    }, [])
-
-    const removeMethod = (method: PaymentMethod, index: number) => {
+    const removeMethod = (method: PaymentMethods, index: number) => {
         console.log("removeMethod", method, index);
+        setLoading(true);
+        authApi(access_token).post(endpoints.remove_payment_method, {
+            "method_id": method.id
+        }).then((res) => {
+            paymentMethods.splice(index, 1);
+            setPaymentMethods([...paymentMethods]);
+        }).catch(ex => {
+            alert(ex.response.data?.error_description || "Delete failed\n" + ex.response.data?.message || "Delete failed\nStatus code" + ex.status)
+        }).finally(() => {
+            setLoading(false);
+        })
     };
 
 
     return (
         <View className={"flex-1"} style={homeStyles.backGround}>
+            {loading && (<LoadingOverlay></LoadingOverlay>)}
             <View className={" align-center flex-1 p-5 items-center"} style={homeStyles.bodyPage}>
                 {paymentMethods.map((method, index) => {
                     return (
@@ -84,7 +115,7 @@ const PaymentMethods = () => {
                                     }}
                                     contentFit="contain"></Image>
                                 <View className={"pl-5 flex-row justify-between items-center"}>
-                                    <Text style={styles.text}>{method.methodName}</Text>
+                                    <Text style={styles.text}>{method.payment_type}</Text>
                                     <View style={{width: 30, height: 30}}>
                                         {(method == defaultMethod || method.isDefault) &&
                                             <IconButton className={""} icon={"check"} iconColor={"green"}></IconButton>}
@@ -99,7 +130,8 @@ const PaymentMethods = () => {
                         </View>
                     )
                 })}
-                <Button buttonColor={Colors.Orange_2} textColor={Colors.Orange_Base} text={"Add new payment method"} onPress={() => router.push("/(payment)/addPaymentMethod")}></Button>
+                <Button buttonColor={Colors.Orange_2} textColor={Colors.Orange_Base} text={"Add new payment method"}
+                        onPress={() => router.push("/(payment)/addPaymentMethod")}></Button>
 
 
             </View>
--- FoodApp/configs/APIs.tsx
+++ FoodApp/configs/APIs.tsx
@@ -7,8 +7,12 @@ export const endpoints = {
     'register': '/users/',
     'login': '/o/token/',
     'roles': '/users/roles/',
-    'get_user': '/users/current-user/',
+    'get_user': '/users/current_user/',
     'logout': '/o/revoke_token/',
+
+    "payment_methods": '/users/payment_methods/',
+    "set_default_payment_method": "/users/set_default_payment_method/",
+    "remove_payment_method": "/users/remove_payment_method/",
 
     'dish': '/dish/',
     'dish_type': '/dish_type/',
@@ -18,20 +22,11 @@ export const endpoints = {
 }
 
 
-export const authApi = async () => {
-    const getOauth2Token = async () => {
-        const oauth2Token = await getObjectValue('oauth2-token');
-        return oauth2Token;
-    }
-
-    const oauth2Token = await getOauth2Token().then((res) => res.access_token);
-
-    // console.log(oauth2Token);
-
+export const authApi = (access_token:string) => {
     return axios.create({
         baseURL: BASE_URL,
         headers: {
-            'Authorization': 'Bearer ' + oauth2Token
+            'Authorization': 'Bearer ' + access_token
         }
     });
 }
