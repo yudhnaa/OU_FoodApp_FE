645025a9a3e73f44d9e8a88426d14cacc3bd0b5d
yudhna <you@example.com>
2025-02-07 22:17:44 +0700

    update

--- FoodApp/app/(store)/(tabs)/index.tsx
+++ FoodApp/app/(store)/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View,Text,Pressable,StyleSheet } from "react-native";
+import { View, Text, Pressable, StyleSheet, Alert } from "react-native";
 import { styles } from "@/components/home/Styles";
 import { Icon } from "react-native-paper";
 import { router } from "expo-router";
@@ -11,25 +11,25 @@ export default function HomePageStore() {
                 <View className="p-5">
                     <View style={styles.seperateLine}></View>
                     <View className="flex-row justify-between items-center p-5">
-                            <Text style = {styles1.txtTitle}>Dish management</Text>
-                            <Pressable onPress={() => router.push('/manage')}>
-                                <Icon 
-                                    source="chevron-right"
-                                    color={"#E95322"}
-                                    size={28}
-                                />
-                            </Pressable>
+                        <Text style={styles1.txtTitle}>Dish management</Text>
+                        <Pressable onPress={() => router.push('/manage')}>
+                            <Icon
+                                source="chevron-right"
+                                color={"#E95322"}
+                                size={28}
+                            />
+                        </Pressable>
                     </View>
-                        {/* <Text style = {styles1.txtDescription}>Support</Text> */}
+                    {/* <Text style = {styles1.txtDescription}>Support</Text> */}
                     <View style={styles.seperateLine}></View>
                 </View>
 
                 <View className="p-5">
                     <View style={styles.seperateLine}></View>
                     <View className="flex-row justify-between items-center p-5">
-                        <Text style = {styles1.txtTitle}>Order management</Text>
+                        <Text style={styles1.txtTitle}>Order management</Text>
                         <Pressable onPress={() => router.push('/order_manage')}>
-                            <Icon 
+                            <Icon
                                 source="chevron-right"
                                 color={"#E95322"}
                                 size={28}
@@ -42,9 +42,9 @@ export default function HomePageStore() {
                 <View className="p-5">
                     <View style={styles.seperateLine}></View>
                     <View className="flex-row justify-between items-center p-5">
-                        <Text style = {styles1.txtTitle}>Statistical Report</Text>
-                        <Pressable onPress={() => router.push('/statistics')}>
-                            <Icon 
+                        <Text style={styles1.txtTitle}>Statistical Report</Text>
+                        <Pressable onPress={() => Alert.alert("Hey Besties", "This feature is developing\nThank you.")}>
+                            <Icon
                                 source="chevron-right"
                                 color={"#E95322"}
                                 size={28}
@@ -61,16 +61,16 @@ export default function HomePageStore() {
 
 const styles1 = StyleSheet.create({
     txtDescription: {
-        fontSize : 14,
-        fontFamily : "Spartan_500Medium",
-        textAlign : "justify",
-        paddingBottom : 15,
+        fontSize: 14,
+        fontFamily: "Spartan_500Medium",
+        textAlign: "justify",
+        paddingBottom: 15,
     },
-    txtDescripContainer : {
-        padding : 15,
+    txtDescripContainer: {
+        padding: 15,
     },
-    txtTitle : {
-        fontSize : 16,
-        fontFamily : "Spartan_700Bold",
+    txtTitle: {
+        fontSize: 16,
+        fontFamily: "Spartan_700Bold",
     }
 })
